a178994c8ecb8af38fb58506dc3829945784acdb
JoanLlompart <jllompartsocias@cifpfbmoll.eu>
2022-11-02 12:48:10 +0100

    8 casi acabat falta colocar

--- src/Exercici8.java
+++ src/Exercici8.java
@@ -2,10 +2,11 @@ public class Exercici8 {
     public static void main(String[] args) {
         Turtle t = new Turtle(800,600);
         //8- Dibuixa el següent gràfic.
-        int longTriangle = 100;
+        int longTriangle = 80;
         int angleGir = 120;
         int numTriangles =4;
 
+        t.setDelay(300);
         for (int i = 0;i<= numTriangles;i++) {
             dibuixaTriangle(t, longTriangle, angleGir);
             //espaiEntreTriangles(t, longTriangle, angleGir);
@@ -27,13 +28,14 @@ public class Exercici8 {
     private static void espaiEntreTriangles(Turtle t,int longTriangle,int angleGir) {
         t.setPenDown(false);
         t.turnLeft(angleGir);
-        t.forward(50);
-        t.turnLeft(90);
-        t.forward(90);
+        t.forward(longTriangle/2);
+        t.turnLeft(angleGir);
+        t.forward(longTriangle/2);
 
         t.turnLeft(180);
+        t.markCursor();
         t.forward(90);
-        //t.turnRight(90);
+        t.turnRight(90);
         t.setPenDown(true);
         //t.markCursor();
 
@@ -43,7 +45,7 @@ public class Exercici8 {
     }
 
     private static void dibuixaTriangle(Turtle t,int longTriangle,int angleGir) {
-        //t.turnRight(90);
+        t.turnRight(30);
         t.forward(longTriangle);
         t.turnRight(angleGir);
         t.forward(longTriangle);
